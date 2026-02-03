e59a4f5491659fd596afae1522af4db935f4205b
JaouedB <jaouedboul@gmail.com>
2026-02-03 14:39:40 +0100

    Add new test route to Flask application

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 from flask import Flask
 app = Flask(__name__)
+
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/test')
+def test_route():
+    return 'Ceci est ma nouvelle route !'
